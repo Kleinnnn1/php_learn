1fe6dc04b2ffb87af56427a719303d11394b5831
Kenn <kennbalino@gmail.com>
2024-02-10 13:01:06 -0800

    basic array

--- index.php
+++ index.php
@@ -1,45 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        <label>username:</label>
-        <input type="text" name="username"><br>
-        <label>password:</label>
-        <input type="password" name="password"><br>
-        <label>email:</label>
-        <input type="text" name="email"><br>
-        <input type="submit" value="start">
-
-    </form>
-</body>
-</html>
-   
-
 <?php
-    echo $_POST["username"] . "<br>";
-    echo $_POST["password"]. "<br>";
-    echo $_POST["email"]. "<br>";
+    $items = array("t1","t2","t3","t4");
 
-//GET METHOD DATA IS APPENDED TO URL NOT SECURE
-// CHAR LIMT
-// BOOK MARK IS POSSIBLE WITH VALUES
-// GET REQUESED CAN BE CACHED
-// BETTER FOR SEARCH PAGE
-// USE GET IF THE DATA IS NOT IMPORTANT
+    foreach($items as $item) {
+        echo $item . "<br>";
+    };
 
+    //$items[0]; select specific items
+    //array_push($foods, "burger"); add items on array
+    //array_pop($items) delete items on first index and adjust the index of all items in the current array
 
-//GET POST DATA IS PACKAGE INSIDE THE BODY OF THE HTTP REQUEST
-// MORE SECURE
-// NO DATA LIMIT
-// CANNOT BOOKMARK
-// GET REQUESED ARE NOT CACHED
-// BETTER FOR SUBMITTING CREDENTIALS
-// USE IF THE DATA IS IMPORTANT
 
 ?>
 
