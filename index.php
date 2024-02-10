06bf42f8fb5b9fd7fc4e4f5cd16867adc8ff8d6b
Kenn <kennbalino@gmail.com>
2024-02-10 14:35:53 -0800

    associative array

--- index.php
+++ index.php
@@ -1,14 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label>enter username:</label>
+        <input type="text" name="username">
+        <input type="submit" value="search">
+    </form>
+</body>
+</html>
+
 <?php
-    $items = array("t1","t2","t3","t4");
 
-    foreach($items as $item) {
-        echo $item . "<br>";
-    };
 
-    //$items[0]; select specific items
-    //array_push($foods, "burger"); add items on array
-    //array_pop($items) delete items on first index and adjust the index of all items in the current array
+   //associative array = key pair values
+    $users = array("Kenn"=> "Male",
+                    "Kc"=> "Female",
+                    "Niel" => "Male"
+);
 
+    $user = $users[$_POST["username"]];
+    echo $user;
+    //$users["Kc"] = "Male";
+    //$users["Kath"] = "Female";
+    //$user_keys_array = array_keys($users);
+
+    /*foreach($users as $key => $value) {
+        echo "{$key} {$value} <br>";
+    }*/
 
 ?>
 
